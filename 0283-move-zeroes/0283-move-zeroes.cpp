987634a083e7594bb6f6395f707372f31f6661ab
Ujjwal Singh <sujjwal171@gmail.com>
2024-09-30 23:27:37 +0530

    Time: 11 ms (90.72%), Space: 21.8 MB (90.21%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int last_non_zero = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[last_non_zero]);
+                last_non_zero++;
+            }
+        }
+    }
+};
