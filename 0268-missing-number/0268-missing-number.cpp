1f968faeae27a3efe705d77d2ab5bdda1d9e5f24
Ujjwal Singh <sujjwal171@gmail.com>
2024-10-08 23:50:56 +0530

    Time: 12 ms (67.07%), Space: 21.1 MB (27.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        map<int,bool> mpp;
-        for(int i = 0 ; i<=n ; i++)
-        {
-            mpp[i]=false;
+        vector<int> arr(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            arr[nums[i]] = 1;
         }
-        for(int i = 0 ; i<n ; i++)
-        {
-            mpp[nums[i]]=true;
-        }
-        for(int i = 0 ; i<=n ; i++)
-        {
-            if(!mpp[i])
-            {
+
+        for (int i = 0; i <= n; i++) {
+            if (arr[i] == 0) {
                 return i;
             }
         }
+
         return -1;
     }
 };
