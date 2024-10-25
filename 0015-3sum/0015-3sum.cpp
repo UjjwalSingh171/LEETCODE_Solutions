c2aebe765284f4a47f919f13a0c5c302ca83c299
Ujjwal Singh <sujjwal171@gmail.com>
2024-10-25 22:38:06 +0530

    Time: 56 ms (79.42%), Space: 27.5 MB (71.39%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,52 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) 
-    {
-        vector<vector<int>> result;
-        
-        // Sort the input array
-        sort(nums.begin(), nums.end());
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         int n = nums.size();
-        
-        // Traverse the array
-        for (int i = 0; i < n - 2; i++) {
-            // Skip duplicates for the first element
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        vector<vector<int>> ans;
+        for(int i = 0 ; i<n-2 ; i++)
+        {
+            if( i>0 and nums[i]==nums[i-1]  )
+            {
                 continue;
             }
-            
-            // Set two pointers
-            int left = i + 1;
-            int right = n - 1;
-            
-            // Find two other numbers that sum up to zero with nums[i]
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if (sum == 0) {
-                    // If we found a triplet that sums up to zero
-                    result.push_back({nums[i], nums[left], nums[right]});
-                    
-                    // Move the left pointer to skip duplicates
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    // Move the right pointer to skip duplicates
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    
-                    // Move both pointers inward
-                    left++;
-                    right--;
-                } 
-                else if (sum < 0) {
-                    // If the sum is less than zero, move left to increase the sum
-                    left++;
-                } 
-                else {
-                    // If the sum is greater than zero, move right to decrease the sum
-                    right--;
+            int j = i+1;
+            int k = n-1;
+            while(j<k)
+            {
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0)
+                {
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    while(j<k and nums[j]==nums[j+1]) j++;
+                    while(j<k and nums[k]==nums[k-1]) k--;
+                    j++;
+                    k--;
+                }
+                else if(sum>0)
+                {
+                    k--;
+                }
+                else
+                {
+                    j++;
                 }
             }
         }
-        
-        return result;
+        return ans;
     }
 };
