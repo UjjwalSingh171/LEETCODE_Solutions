9370a4263c116760adbf8a024b1b2f2cdc7f6482
Ujjwal Singh <sujjwal171@gmail.com>
2025-05-23 14:11:49 +0530

    Time: 0 ms (100%), Space: 43.6 MB (41.97%) - LeetHub

--- 2621-find-xor-beauty-of-array/2621-find-xor-beauty-of-array.cpp
+++ 2621-find-xor-beauty-of-array/2621-find-xor-beauty-of-array.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int xorBeauty(vector<int>& nums) {
         int n = nums.size();
-        int result = 0 ;
-        for(int i = 0 ; i<n ; i++ )
+        int res = 0 ;
+        for(int i = 0 ; i<n ; i++)
         {
-            result ^= nums[i];
+            res ^= nums[i];
         }
-        return result;
+        return res;
     }
 };
