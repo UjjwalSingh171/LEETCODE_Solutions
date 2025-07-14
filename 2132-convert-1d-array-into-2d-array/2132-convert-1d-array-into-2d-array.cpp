1e7b4369d9137d30b79893c5d5cda853d0d60e4f
Ujjwal Singh <sujjwal171@gmail.com>
2025-07-14 20:28:18 +0530

    Time: 3 ms (99.17%), Space: 89.8 MB (96.19%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if(original.size()!=m*n) return {};
 
-        if(m*n != original.size())
-        {
-            return {};
-        }
+        vector<vector<int>> prime(m,vector<int>(n)) ;
+        int k = 0 ;
 
-        vector<vector<int>> product(m, vector<int>(n));
-        int x = 0;
         for(int i = 0 ; i<m ; i++)
         {
             for(int j = 0 ; j<n ; j++)
             {
-                product[i][j]=original[x++];
+                prime[i][j]=original[k++];
             }
         }
-        return product;
+        return prime;
     }
 };
