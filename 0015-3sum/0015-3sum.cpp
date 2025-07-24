eac52a8ebba9a0c817d598ecee229304bc0cacd2
Ujjwal Singh <sujjwal171@gmail.com>
2025-07-24 10:45:58 +0530

    Time: 51 ms (51.99%), Space: 29.1 MB (50.26%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,36 +3,29 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        vector<vector<int>> res;
+        vector<vector<int>> v;
+        for(int i = 0 ; i<n-2 ; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            for(int i = 0 ; i < n-2 ; i++)
+            int start = i+1 ;
+            int end = n-1 ;
+            while(start<end)
             {
-                if (i > 0 && nums[i] == nums[i - 1]) continue; 
+                int sum = nums[i]+nums[start]+nums[end];
+                if(sum==0){
+                    v.push_back({nums[i],nums[start],nums[end]});
 
-                int left = i+1;
-                int right = n-1;
+                    while (start < end && nums[start] == nums[start + 1]) start++;
+                    while (start < end && nums[end] == nums[end - 1]) end--;
 
-                while(left<right)
-                {
-                    int sum = nums[i]+nums[left]+nums[right];
-
-                    if(sum==0)
-                    {
-                        res.push_back({nums[i], nums[left], nums[right]});
-
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
-
-                        left++;
-                        right--;
-                    }
-
-                    else if(sum<0) left++;
-                    else right--;
+                    start++;
+                    end--;
                 }
-
-                
+                else if(sum<0) start++;
+                else end--;
             }
-        return res;
+        }
+        return v;
     }
 };
