d9b5fe7633ac1e8b1cbfc14cff0cd03ef940df6a
Ujjwal Singh <sujjwal171@gmail.com>
2025-08-15 00:29:42 +0530

    Time: 47 ms (70.11%), Space: 29 MB (89.78%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
+
         sort(nums.begin(),nums.end());
-        vector<vector<int>> v;
-        for(int i = 0 ; i<n-2 ; i++)
-        {
+        int n = nums.size();
+        vector<vector<int>> triplets;
+
+        for(int i = 0 ; i<n-2 ; i++){
+
             if (i > 0 && nums[i] == nums[i - 1]) continue;
+    
+            int left = i+1 ;
+            int right = n-1 ;
 
-            int start = i+1 ;
-            int end = n-1 ;
-            while(start<end)
-            {
-                int sum = nums[i]+nums[start]+nums[end];
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
                 if(sum==0){
-                    v.push_back({nums[i],nums[start],nums[end]});
-
-                    while (start < end && nums[start] == nums[start + 1]) start++;
-                    while (start < end && nums[end] == nums[end - 1]) end--;
-
-                    start++;
-                    end--;
+                    triplets.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
                 }
-                else if(sum<0) start++;
-                else end--;
+                else if(sum<0) left++;
+                else right--;
             }
         }
-        return v;
+        return triplets;
     }
 };
